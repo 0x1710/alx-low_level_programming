c020341d9c5dc1e788a440ef631437c3d142cd04
Yassine <106806639+0x1710@users.noreply.github.com>
2022-07-07 12:58:42 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX School students.
  *
  * Return: void.
  */
@@ -38,3 +38,5 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
+
+}
